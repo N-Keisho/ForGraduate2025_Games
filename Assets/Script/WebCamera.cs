4bd80e2b11e06c52d16a1bad62133bfe33d21313
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-01-12 00:30:25 +0900

    Webカメラ機能を付けたけどめっちゃ重いうえに写らない
    外部のカメラだとダメなのかも？

---
+++ Assets/Script/WebCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamera : MonoBehaviour {
+
+    [SerializeField] RawImage rawImage;
+    [SerializeField] int devicesIndex = 0;
+    int width = 1920;
+    int height = 1080;
+    int fps = 30;
+    WebCamTexture webcamTexture;
+    
+
+    void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(devices[devicesIndex].name, this.width, this.height, this.fps);
+        rawImage.texture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
